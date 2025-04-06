19c354fa7738f1e5cc9cc31f4859969222485f21
akbar <akbar4857@qq.com>
2025-04-06 16:59:36 +0800

    文章目录功能已实现

--- src/data/nav-data.ts
+++ src/data/nav-data.ts
@@ -1,4 +1,5 @@
 const navData = [
+  {name: 'Home', href: '/', icon: 'mdi:home' },
   { name: 'Project', href: '/project', icon: 'mdi:microsoft-visual-studio-code' },
   { name: 'About', href: '/about', icon: 'mdi:alert-circle' },
 ]
